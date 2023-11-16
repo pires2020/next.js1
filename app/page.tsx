ff0e12ea202b3bbcf1da44dc5ecb6653671af30e
Lucas Antunes Pires <64935655+pires2020@users.noreply.github.com>
2023-11-16 16:41:58 -0300

    a

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+<>
+<header className='bg-black h-14 outline p-4 items-center flex flex-row justify-between'>
+ <h1 className='text-blue-900 text-3xl pr-[50%]'>Home</h1>
+ <div className='text-blue-900 text-2xl flex justify-between w-[600px] p-10'>
+  <a href="">About</a>
+  <a href="">Docs</a>
+  <a href="">Contact</a>
+ </div>
+ <button className='text-blue-900 outline rounded-full p-3 pt-2'>Start</button>
+</header>
+
+<div></div>
+
+<section></section>
+
+<section></section>
+
+
+<footer></footer>
+</>
+  )
+}
